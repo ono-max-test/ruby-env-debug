3b2122d3dea19b9f0b1dc761fa39b126fcef1405
Jun Aruga <jaruga@redhat.com>
2019-09-26 18:49:57 +0200

    Add time command to measure each command's time.

--- tool/ci/test.sh
+++ tool/ci/test.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
+NPROC="$(nproc)"
+JOBS="-j$((1+${NPROC}))"
+TESTOPTS="${TESTOPTS=${JOBS} -q --tty=no}"
+TIME="/usr/bin/time -f TIME=%esec"
+
 autoconf
-./configure --disable-install-doc --prefix=/tmp/ruby-prefix
-make -s -j install
-make -s test
-make -s test-all
-make -s test-spec
+${TIME} ./configure --disable-install-doc --prefix=/tmp/ruby-prefix
+${TIME} make -s ${JOBS}
+${TIME} make install
+${TIME} make -s test -o showflags TESTOPTS="${TESTOPTS}"
+${TIME} make -s test-all TESTOPTS="${TESTOPTS}"
+${TIME} make -s test-spec MSPECOPT=-ff
+${TIME} make -s -o showflags leaked-globals
 
 exit 0

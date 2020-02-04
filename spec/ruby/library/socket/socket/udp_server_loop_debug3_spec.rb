84a8e210e581b1e426762f73f6cd7b8444a33c68
Jun Aruga <jaruga@redhat.com>
2020-02-05 00:20:28 +0100

    Update udp_server_loop_debug3_spec.rb

--- spec/ruby/library/socket/socket/udp_server_loop_debug3_spec.rb
+++ spec/ruby/library/socket/socket/udp_server_loop_debug3_spec.rb
@@ -1,9 +1,15 @@
-require_relative '../spec_helper2'
-# require_relative '../fixtures/classes'
+require_relative '../../../spec_helper'
+require 'socket'
 
 describe 'Socket.udp_server_loop debug' do
   it 'blocks the caller' do
-    socket_block = -> { Socket.udp_server_loop('127.0.0.1', 0) }
+    # socket_block = -> { Socket.udp_server_loop('127.0.0.1', 0) }
+    socket_block = -> do
+      Socket.udp_server_sockets('127.0.0.1', 0) do |sockets|
+        Socket.udp_server_loop_on(sockets)
+      end
+    end
+
     socket_block.should block_caller
   end
 end

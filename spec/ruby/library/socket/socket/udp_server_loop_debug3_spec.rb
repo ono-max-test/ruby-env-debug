7f5382b5d8b693a6e8b3d5ed9e0ff449efc09309
Jun Aruga <jaruga@redhat.com>
2020-02-05 00:45:30 +0100

    Update udp_server_loop_debug3_spec.rb with only Socket.ip_address_list.

--- spec/ruby/library/socket/socket/udp_server_loop_debug3_spec.rb
+++ spec/ruby/library/socket/socket/udp_server_loop_debug3_spec.rb
@@ -3,14 +3,11 @@ require 'socket'
 
 describe 'Socket.udp_server_loop debug' do
   it 'blocks the caller' do
-    # socket_block = -> { Socket.udp_server_loop('127.0.0.1', 0) }
-    socket_block = -> do
-      # Socket.udp_server_sockets('127.0.0.1', 0) do |sockets|
-      #   Socket.udp_server_loop_on(sockets)
-      # end
-      Socket.ip_address_list
-    end
+    # socket_block = -> do
+    #   Socket.ip_address_list
+    # end
 
-    socket_block.should block_caller
+    # socket_block.should block_caller
+    Socket.ip_address_list
   end
 end

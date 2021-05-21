947912d37b03ce876c5bdf85f3b71d390568a757
Jun Aruga <jaruga@redhat.com>
2021-05-21 13:56:35 +0200

    .travis.yml: Improve.

--- tool/ci_functions.sh
+++ tool/ci_functions.sh
@@ -1,6 +1,11 @@
 # -*- BASH -*-
+# Manage functions used on a CI.
+# Run `. tool/ci_functions.sh` to use it.
 
-function ci_to_excluded_tests {
+# Create options with patterns `-n !/name1/ -n !/name2/ ..` to exclude the test
+# method names by the method names `name1 name2 ..`.
+# See `ruby tool/test/runner.rb --help` `-n` option.
+function ci_to_excluded_test_opts {
   local tests_str="${1}"
   ruby <<EOF
     skipped_tests = "${tests_str}".split.map { |test| "-n \!/#{test}\$$/" }
@@ -9,7 +14,10 @@ EOF
   return 0
 }
 
-function ci_to_included_tests {
+# Create options with patterns `-n /name1/ -n /name2/ ..` to exclude the test
+# method names by the method names `name1 name2 ..`.
+# See `ruby tool/test/runner.rb --help` `-n` option.
+function ci_to_included_test_opts {
   local tests_str="${1}"
   ruby <<EOF
     skipped_tests = "${tests_str}".split.map { |test| "-n #{test}" }
